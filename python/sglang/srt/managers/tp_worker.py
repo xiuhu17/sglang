d1aea1e10b9439ce254a3de0c61609f8e2160c43
zhihaow6 <zhihaow6@illinois.edu>
2026-05-25 22:42:14 -0700

    update
    Signed-off-by: zhihaow6 <zhihaow6@illinois.edu>

--- python/sglang/srt/managers/tp_worker.py
+++ python/sglang/srt/managers/tp_worker.py
@@ -158,9 +158,10 @@ class BaseTpWorker(ABC):
     def update_weights_from_tensor(self, recv_req: UpdateWeightsFromTensorReqInput):
 
         monkey_patch_torch_reductions()
+        tensor_rank = self.tp_rank
         success, message = self.model_runner.update_weights_from_tensor(
             named_tensors=MultiprocessingSerializer.deserialize(
-                recv_req.serialized_named_tensors[self.tp_rank]
+                recv_req.serialized_named_tensors[tensor_rank]
             ),
             load_format=recv_req.load_format,
         )
